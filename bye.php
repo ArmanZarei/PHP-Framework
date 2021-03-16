492651d2725a1dc0669d495dcbb01d446a3ef37e
Arman <armanzarei1378@gmail.com>
2021-03-16 16:43:28 +0330

    Front Controller

--- bye.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__.'/init.php';
-
-$response->setContent('Bye');
-$response->send();
---
+++ src/pages/hello.php
@@ -0,0 +1,1 @@
+<h4>Hello <strong><?php echo $request->get('name', 'Unknown'); ?></strong></h4>
